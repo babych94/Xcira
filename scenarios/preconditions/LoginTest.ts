994fe443b52719f56dd08e175b21cc5528d6d77a
babych <vasyl.babych@swanlogic.com>
2017-04-25 18:07:29 +0300

    babychBranch -small changes

--- scenarios/preconditions/LoginTest.ts
+++ scenarios/preconditions/LoginTest.ts
@@ -4,19 +4,19 @@
 import {browser, element, by, By, $, $$, ExpectedConditions as EC} from 'protractor';
 
 import LoginPage = require("../../page_object/LoginPage");
-//import VehiclesPage = require("../../page_object/VehiclesPage");
+import VehiclesPage = require("../../page_object/VehiclesPage");
 describe('ABC FastLane Testing', function () {
     let loginPage = new LoginPage;
-   // let vehiclesPage = new VehiclesPage;
+    let vehiclesPage = new VehiclesPage;
 
     it ('open login page and enter user credentials', function () {
-        browser.get('/sign-in');
+        browser.get(process.env.BASE_URL);
         expect(loginPage.loginPageIsDisplayed()).toBeTruthy("check that login page is open");
         loginPage.loginAs(process.env.ADMIN_EMAIL, process.env.ADMIN_PSW);
     });
 
-    // it("Check that user was logged in", function () {
-    //     expect(vehiclesPage.userIsLoggedIn()).toBeTruthy("Should be displayed profile icon");
-    //     expect(browser.getCurrentUrl()).toBe(browser.baseUrl+vehiclesPage.url);
-    // })
+    it("Check that user was logged in", function () {
+        expect(vehiclesPage.userIsLoggedIn()).toBeTruthy("Should be displayed profile icon");
+        expect(vehiclesPage.getPageUrl()).toContain(browser.baseUrl + "/#" + vehiclesPage.url);
+    })
 });
