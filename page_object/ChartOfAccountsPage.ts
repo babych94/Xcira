618ad1e826c74b7ce56ce30eac67543c7b5219e1
egorkamenca <egorkamenca@gmail.com>
2017-05-10 17:07:51 +0300

    all tests added

--- page_object/ChartOfAccountsPage.ts
+++ page_object/ChartOfAccountsPage.ts
@@ -14,8 +14,25 @@ class ChartOfAccountsPage extends PageAbstract{
     FilterButton = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[1]/div/div[1]/div/div/div[1]/div/div/span"));
     FilterAccountNamberField = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div/input"));
     FilterApplyButton = element(by.css(".apply_filters_container .container_buttons .fl-buttonContainer:last-child"));
-    AccountFirstItem = element(by.css(".ag-body-container .ag-row .ag-cell"));
+    FilterAccountTypeDropdown = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[1]/div/div[1]/div/div/div[2]/div/div[1]/div/div[2]/div[1]"));
+    FilterAsset = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[1]/div/div[1]/div/div/div[2]/div/div[1]/div/div[2]/div[3]/div[2]/div[1]"));
+    FilterEquity = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[1]/div/div[1]/div/div/div[2]/div/div[1]/div/div[2]/div[3]/div[2]/div[2]"));
+    FilterExpense = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[1]/div/div[1]/div/div/div[2]/div/div[1]/div/div[2]/div[3]/div[2]/div[3]"));
+    FilterLiability = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[1]/div/div[1]/div/div/div[2]/div/div[1]/div/div[2]/div[3]/div[2]/div[4]"));
+    FilterRevenue = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[1]/div/div[1]/div/div/div[2]/div/div[1]/div/div[2]/div[3]/div[2]/div[5]"));
+    AccountFirstItemInList = element(by.css(".ag-body-container .ag-row .ag-cell"));
+    AccountFirstAccountNameInList = element(by.xpath("//*[@id='center']/div/div[4]/div[3]/div/div/div[1]/div[2]"));
+    AccountFirstAccountTypeInList = element.all(by.css(".title-data-container"));
     CancelButton = element(by.xpath(".//form/div[4]/button[1]"));
+    AccountSummeryTitle = element(by.className("fiscal-toolbar-info-header"));
+    DateofLastTransfer = element.all(by.className("fiscal-toolbar-info-sub-header-info"));
+    ThreePointIcon = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[2]/div/div/div/div/div/div[1]/div/div[4]/div[2]/div/div/div[2]/div/div/span"));
+    EditAccountButton = element(by.xpath("//*[@id='center']/div/div[4]/div[2]/div/div[1]/div[2]/div/div/div/div/h4[1]"));
+    EditAccountAccountNameField = element(by.xpath("//*[@id='root']/div/div[3]/div[2]/div/div[2]/div[3]/div[4]/div[2]/div[2]/div/input"));
+    EditAccountSaveButton = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[4]/div[2]/div[2]/div/div/button[2]"));
+    DisableAccountButton = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[5]/div[2]/div/div/div/div/div/div[1]/div/div[4]/div[2]/div/div/div[2]/div/div/div/div/h4[2]"));
+    DisableAccountDisableButton = element(by.xpath("html/body/div/div/div[3]/div[2]/div/div[2]/div[3]/div[3]/div[2]/div[2]/div/div/div[3]/button[2]"));
+
 
     setAccountName(AccountName: string){
         this.AccountNameField.sendKeys(AccountName)
@@ -30,6 +47,10 @@ class ChartOfAccountsPage extends PageAbstract{
     setFilterAccountNamber(AccountNamber: string){
         this.FilterAccountNamberField.sendKeys(AccountNamber)
     };
+
+    setEditAccountAccountNameField(AccountNamber: string){
+        this.EditAccountAccountNameField.sendKeys(AccountNamber)
+    }
 
     AddAccount(AccountName: string,AccountNumber: string,BeginningBalance: string){
         this.AddAccountButton.click();
@@ -62,7 +83,7 @@ class ChartOfAccountsPage extends PageAbstract{
     };
 
     TableIsEmpty(){
-       return this.AccountFirstItem.isPresent();
+       return this.AccountFirstItemInList.isPresent();
     }
 
     FindedAccountCheck(AccountName: string){
@@ -70,17 +91,83 @@ class ChartOfAccountsPage extends PageAbstract{
         this.setFilterAccountNamber(AccountName);
         this.FilterApplyButton.click();
 
-        browser.sleep(10000);
-    }
-
-    CheckAccountAdded(test: string){
-     //   return  this.AccountFirstItem.getText().equal(test);
+        browser.sleep(5000);
     }
 
     ChartOfAccountsPageIsDisplayed(){
-        return  this.elementIsDisplayed(this.AccountFirstItem);
+        return  this.elementIsDisplayed(this.AccountFirstItemInList);
+    }
+
+    OpenAccountSummeryFilterAccountTypeDropdown(){
+        this.FilterButton.click();
+        this.FilterAccountTypeDropdown.click();
+    }
+
+    ChooseFilterAccountTypeAsset(){
+        this.FilterAsset.click();
+    }
+
+    ChooseFilterAccountTypeEquity(){
+        this.FilterEquity.click();
+    }
+
+    ClickFilterApplyButton(){
+        this.FilterApplyButton.click()
+    }
+
+    ClickThreePointIconInFirstRowInList(){
+        this.ThreePointIcon.click();
+    }
+
+    ClickEditAccountButton(){
+        this.EditAccountButton.click();
+    }
+
+    ClicEditAccountSaveButton(){
+        this.EditAccountSaveButton.click();
+    }
+
+    ClickDisableAccountButton(){
+        this.DisableAccountButton.click();
+    }
+
+    ClickDisableAccountDisableButton(){
+        this.DisableAccountDisableButton.click();
+        browser.sleep(2000);
+    }
+
+    Checkitem1(){
+        return this.FilterAsset.getText().then((data) => {return data == ("Asset")});
+    }
+    Checkitem2(){
+        return this.FilterEquity.getText().then((data) => {return data == ("Equity")});
+    }
+    Checkitem3(){
+        return this.FilterExpense.getText().then((data) => {return data == ("Expense")});
+    }
+    Checkitem4(){
+        return this.FilterLiability.getText().then((data) => {return data == ("Liability")});
+    }
+    Checkitem5(){
+        return this.FilterRevenue.getText().then((data) => {return data == ("Revenue")});
+    }
+
+    ChecFilterAccountTypewithEquityItem(){
+        browser.sleep(2000);
+        return this.AccountFirstAccountTypeInList.first().getText().then((data) => {return data == ("Equity")});
+    }
+
+    ChecFilterAccountTypewithAssetItem(){
+        browser.sleep(2000);
+        return this.AccountFirstAccountTypeInList.first().getText().then((data) => {return data == ("Asset")});
+    }
+    CheckActiveAccountsAccountSummerySectionTitle(){
+        return this.AccountSummeryTitle.getText().then((data) => {return data == ("Account Summary")});
+    }
+
+    CheckActiveAccountsAccountSummerySectionDateofLastTransfer(){
+        return this.DateofLastTransfer.last().getText().then((data) => {return data == ("3/2/2017 by nrichards")});
     }
 }
-
 
 export = ChartOfAccountsPage;
--- scenarios/ChartOfAccounts/ChartOfAccountsTest.ts
+++ scenarios/ChartOfAccounts/ChartOfAccountsTest.ts
@@ -14,13 +14,64 @@ describe('ABC FastLane Testing', function () {
     it ('Add Account', function () {
         chartOfAccounts.AddAccount(name.toLocaleString(), number.toLocaleString(), "123.00");
         chartOfAccounts.FindedAccountCheck(name.toLocaleString());
-        chartOfAccounts.AccountFirstItem.getText().then((data) => expect(data).toEqual(number.toLocaleString()));
+        chartOfAccounts.AccountFirstItemInList.getText().then((data) => expect(data).toEqual(number.toLocaleString()));
     });
 
     it ('Cancel Adding Account', function () {
         chartOfAccounts.CancelAddingAccount((name+200).toLocaleString(), (number+200).toLocaleString(), "123.00");
         chartOfAccounts.FindedAccountCheck((name+200).toLocaleString());
         expect(chartOfAccounts.TableIsEmpty()).toBeFalsy("check that Cancel Adding Account");
+    });
+
+    it ('Account Summary block', function () {
+        expect(chartOfAccounts.CheckActiveAccountsAccountSummerySectionTitle()).toBeTruthy("check Account Summery title");
+        expect(chartOfAccounts.CheckActiveAccountsAccountSummerySectionDateofLastTransfer()).toBeTruthy("check Account Summery Date of Last Transfer");
+
+    });
+
+    it ('Filter data in the grid by Account Type', function () {
+        chartOfAccounts.OpenAccountSummeryFilterAccountTypeDropdown();
+        expect(chartOfAccounts.Checkitem1()).toBeTruthy("check item 1");
+        expect(chartOfAccounts.Checkitem2()).toBeTruthy("check item 2");
+        expect(chartOfAccounts.Checkitem3()).toBeTruthy("check item 3");
+        expect(chartOfAccounts.Checkitem4()).toBeTruthy("check item 4");
+        expect(chartOfAccounts.Checkitem5()).toBeTruthy("check item 5");
+        chartOfAccounts.ChooseFilterAccountTypeEquity();
+        chartOfAccounts.ClickFilterApplyButton();
+        expect(chartOfAccounts.ChecFilterAccountTypewithEquityItem()).toBeTruthy("check filter");
+    });
+
+    it ('Filter data in the grid by Account Name', function () {
+        chartOfAccounts.FindedAccountCheck(name.toLocaleString());
+        chartOfAccounts.AccountFirstAccountNameInList.getText().then((data) => expect(data).toEqual(name.toLocaleString()));
+    });
+
+    it ('Filter data in the grid by Account Type and Account Name', function () {
+        chartOfAccounts.OpenAccountSummeryFilterAccountTypeDropdown();
+        chartOfAccounts.ChooseFilterAccountTypeAsset();
+        chartOfAccounts.setFilterAccountNamber(name.toLocaleString());
+        chartOfAccounts.ClickFilterApplyButton();
+        chartOfAccounts.AccountFirstAccountNameInList.getText().then((data) => expect(data).toEqual(name.toLocaleString()));
+        expect(chartOfAccounts.ChecFilterAccountTypewithAssetItem()).toBeTruthy("check filter");
+    });
+
+    it ('Edit Account', function () {
+        chartOfAccounts.FindedAccountCheck(name.toLocaleString());
+        chartOfAccounts.ClickThreePointIconInFirstRowInList();
+        chartOfAccounts.ClickEditAccountButton();
+        chartOfAccounts.setEditAccountAccountNameField("Test Editing");
+        chartOfAccounts.ClicEditAccountSaveButton();
+        browser.sleep(2000);
+        chartOfAccounts.AccountFirstAccountNameInList.getText().then((data) => expect(data).toEqual(name.toLocaleString() + "Test Editing"));
+    });
+
+    it ('Disable Account', function () {
+        chartOfAccounts.FindedAccountCheck(name.toLocaleString() + "Test Editing");
+        chartOfAccounts.ClickThreePointIconInFirstRowInList();
+        chartOfAccounts.ClickDisableAccountButton();
+        chartOfAccounts.ClickDisableAccountDisableButton();
+        chartOfAccounts.FindedAccountCheck(name.toLocaleString() + "Test Editing");
+        expect(chartOfAccounts.TableIsEmpty()).toBeFalsy("account is disable");
     });
 
 
